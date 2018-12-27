ba997a9249e4f129bd8809bbb3d14d681bb96ff6
Karlos Santos <lgvichy@gmail.com>
2018-12-27 14:42:20 -0800

    Separate framework into smaller modules

---
+++ Source/NujasCore/Private/NujasCoreModule.cpp
@@ -0,0 +1,20 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "NujasCoreModule.h"
+
+#define LOCTEXT_NAMESPACE "FNujasCoreModule"
+
+void FNujasCoreModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FNujasCoreModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FNujasCoreModule, NujasCore)
