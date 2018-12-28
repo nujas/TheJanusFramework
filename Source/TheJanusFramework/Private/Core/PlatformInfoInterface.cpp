0f9446550a1de131495e4f398aae8ae4ed55e2ca
Karlos Santos <lgvichy@gmail.com>
2018-12-27 17:04:17 -0800

    Clean-up post merge

--- Source/TheJanusFramework/Private/Core/PlatformInfoInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Core/PlatformInfoInterface.h"
-
-
-// Add default functionality here for any IPlatformInfoInterface functions that are not pure virtual.
