381278fac5254e64b27a1f257b3d18076393a4ff
Dennis Dennis <dennis.slavinsky@gmail.com>
2019-02-08 00:41:48 -0700

    Continued Refactor of the Rotation System in tick

--- Source/NujasAnimation/Private/CharacterAnimationUtilityComponent.cpp
+++ Source/NujasAnimation/Private/CharacterAnimationUtilityComponent.cpp
@@ -3,6 +3,7 @@
 #include "CharacterAnimationUtilityComponent.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Runtime/Core/Public/Misc/App.h"
 
 UCharacterAnimationUtilityComponent::UCharacterAnimationUtilityComponent()
 {
@@ -25,22 +26,17 @@ void UCharacterAnimationUtilityComponent::UpdateCharacterRotationBasedOnMovement
 {
 	if (ChracterMovementComponent)
 	{
-		if (!FMath::IsNearlyZero(VerticalInput, 0.01f) || !FMath::IsNearlyZero(HorizontalInput, 0.01f))
-		{
-			ChracterMovementComponent->bUseControllerDesiredRotation = true;
-		}
-		else
-		{
-			ChracterMovementComponent->bUseControllerDesiredRotation = false;
-		}
+		ChracterMovementComponent->bUseControllerDesiredRotation =
+			!FMath::IsNearlyZero(VerticalInput, 0.01f) ||
+			!FMath::IsNearlyZero(HorizontalInput, 0.01f);
 	}
 	else
 	{
 		UE_LOG
 		(
-			LogTemp, 
+			LogTemp,
 			Error,
-			TEXT("The Owner's movement component could not be accessed in %s"), 
+			TEXT("The Owner's movement component could not be accessed in %s"),
 			*this->GetName()
 		);
 	}
@@ -48,14 +44,77 @@ void UCharacterAnimationUtilityComponent::UpdateCharacterRotationBasedOnMovement
 
 bool UCharacterAnimationUtilityComponent::IsCharacterMoving()
 {
-	if(CharacterOwner)
-		return CharacterOwner->GetVelocity().IsNearlyZero(0.01f) ? false : true;
+	if (CharacterOwner)
+		return !CharacterOwner->GetVelocity().IsNearlyZero(0.01f);
 	return false;
 }
 
 bool UCharacterAnimationUtilityComponent::IsThereMovementInput()
 {
 	if (ChracterMovementComponent)
-		return ChracterMovementComponent->GetLastInputVector().IsNearlyZero(0.001f) ? false : true;
+		return !ChracterMovementComponent->GetLastInputVector().IsNearlyZero(0.001f);
 	return false;
+}
+
+void UCharacterAnimationUtilityComponent::UpdateForwardBasedAnimationSystem(float VerticalInput, float HorizontalInput)
+{
+	UpdateCharacterRotationBasedOnMovement(VerticalInput, HorizontalInput);
+	UpdateAnimationData();
+}
+
+void UCharacterAnimationUtilityComponent::UpdateAnimationData()
+{
+	SetCurrentSpeed();
+	UpdateInAirData();
+	AnimationUtilData.bCharacterIsMoving = IsCharacterMoving();
+	AnimationUtilData.bReceivingPlayerInput = IsThereMovementInput();
+}
+
+void UCharacterAnimationUtilityComponent::UpdateInAirData()
+{
+	if(ChracterMovementComponent)
+	{
+		if (ChracterMovementComponent->IsFalling())
+		{
+			if (CharacterOwner)
+			{
+				float FallSpeed = CharacterOwner->GetVelocity().Z;
+				float JumpVelocity = AnimationUtilData.JumpVelocity;
+				AnimationUtilData.FallSpeed = FallSpeed;
+				AnimationUtilData.InAirTime += FApp::GetDeltaTime();
+				float ClampedFallSpeed = FMath::GetMappedRangeValueClamped
+				(
+					{ JumpVelocity, -1 * JumpVelocity },
+					{ 1, -1 },
+					FallSpeed
+				);
+				float NormalizedInRangeSpeed = FMath::Clamp
+				(
+					AnimationUtilData.MovementSpeed,
+					0.f,
+					ChracterMovementComponent->MaxWalkSpeed
+				);
+				AnimationUtilData.LeanInAir = ClampedFallSpeed * NormalizedInRangeSpeed;
+			}
+		}
+		else
+		{
+			AnimationUtilData.InAirTime = 0;
+			AnimationUtilData.FallSpeed = 0;
+			AnimationUtilData.LeanInAir = 0;
+		}
+	}
+}
+
+void UCharacterAnimationUtilityComponent::SetCurrentSpeed()
+{
+	if (CharacterOwner)
+	{
+		AnimationUtilData.MovementSpeed = CharacterOwner->GetVelocity().Size();
+	}
+}
+
+void UCharacterAnimationUtilityComponent::SetCurrentDirection()
+{
+	// TODO: Rotations Calc Necessary in order to correctly predict direction
 }
