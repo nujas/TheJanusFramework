13563881db54a4f7ef17a834797c9c19ce8fc76f
Dennis Dennis <dennis.slavinsky@gmail.com>
2019-06-19 01:05:28 -0700

    added final touches to the sensing component

--- Source/NujasCombat/Public/TargetSensingComponent.h
+++ Source/NujasCombat/Public/TargetSensingComponent.h
@@ -12,7 +12,31 @@ class UBoxComponent;
 class UCameraComponent;
 
 const float SQUARED_DISTANCE_SENSING_RANGE = 800000.f;
+const float SCREEN_RANGE = 0.15f;
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FEnemyDetectionDelegate);
+
+
+USTRUCT()
+struct FSensingDistanceResponse
+{
+	GENERATED_BODY()
+
+	bool bIsInRange;
+	float DistSquaredFromSensorToTarget;
+};
+
+USTRUCT()
+struct FSensingViewSpaceResponse
+{
+	GENERATED_BODY()
+
+	bool bIsInView;
+	float DotProductFromViewToTarget;
+};
+
+// This is a sensor that would point in the direction of enemies around.
+// Implementation TBA
 UCLASS(ClassGroup = (Custom), meta = (Blue))
 class NUJASCOMBAT_API AActorSensor : public AActor
 {
@@ -22,9 +46,6 @@ public:
 	AActorSensor();
 protected:
 	virtual void BeginPlay() override;
-public:
-	virtual void Tick(float DeltaSeconds) override;
-
 };
 
 
@@ -33,16 +54,20 @@ class NUJASCOMBAT_API UTargetSensingComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	TSharedPtr<UCameraComponent> PlayerCamera = nullptr; // cannot be assigned as a property
 	UPROPERTY()
-	TSharedPtr<UCameraComponent> PlayerCamera = nullptr;
+	AActor* Owner = nullptr;
 	UPROPERTY()
 	AActor* ClosestEnemy = nullptr;
 	UPROPERTY()
+	float DotProdToClosestEnemy = NAN;
+	UPROPERTY()
+	float DistSqrdToClosestEnemy = NAN;
+	UPROPERTY()
 	TMap<AActor*, AActorSensor*> SensorMap;
 
-
-	bool IsActorWithinScreenRange(AActor* Enemy, float& DotProduct); // idk why have the dot product yet
-	bool IsWithinSensingRange(float SquaredDistanceToEnemy);
+	inline FSensingViewSpaceResponse IsEnemyInScreenView(const AActor* Enemy);
+	inline FSensingDistanceResponse IsEnemyInSensingDistance(const AActor* Enemy);
 
 public:
 	// Sets default values for this component's properties
@@ -50,6 +75,13 @@ public:
 
 	UPROPERTY(BlueprintReadWrite, Category="Target Sensing")
 	float SquaredDistanceSensingRange = SQUARED_DISTANCE_SENSING_RANGE;
+	UPROPERTY(BlueprintReadWrite, Category="Target Sensing")
+	float ScreenRange = SCREEN_RANGE;
+
+	UPROPERTY(BlueprintReadWrite, Category="Target Sensing")
+	FEnemyDetectionDelegate OnEnemyEnter;
+	UPROPERTY(BlueprintReadWrite, Category="Target Sensing")
+	FEnemyDetectionDelegate OnEnemyExit;
 
 protected:
 	// Called when the game starts
@@ -59,9 +91,14 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
 
+	UFUNCTION(BlueprintCallable, Category="Target Sensing")
 	void AddEnemy(AActor* Enemy);
+	UFUNCTION(BlueprintCallable, Category="Target Sensing")
 	void RemoveEnemy(AActor* Enemy);
+	UFUNCTION(BlueprintCallable, Category="Target Sensing")
 	AActor* SearchForClosestEnemy();
-	AActor* GetClosestEnemy();
-	int32 EnemyCountInBounds();
+	UFUNCTION(BlueprintPure, Category="Target Sensing")
+	inline AActor* GetClosestEnemy();
+	UFUNCTION(BlueprintPure, Category="Target Sensing")
+	inline int32 GetEnemyNumInBounds();
 };
